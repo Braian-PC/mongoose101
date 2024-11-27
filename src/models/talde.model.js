7aba73b4905add2c7050779e7d6839fbcd22ecb4
Braian <157797477+Braian-PC@users.noreply.github.com>
2024-11-27 20:22:36 +0000

    Finish

---
+++ src/models/talde.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const taldeSchema = new mongoose.Schema({
+   izena: String,
+   ikasleak: [{
+     type: mongoose.Schema.Types.ObjectId,
+     ref: 'Ikasle'
+   }]
+ });
+
+module.exports = mongoose.model('Taldea', taldeSchema);
